1ac5f0665f28aee1cf042e9b3e5488d3a0e8bf3d
Ravikant Sharma <rksharma1401@hotmail.com>
2019-01-11 19:44:19 +0530

    Add files via upload

---
+++ src/main/java/springboot/config/StartUpController.java
@@ -0,0 +1,59 @@
+package springboot.config;
+
+import javax.annotation.PreDestroy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableAsync
+// @Controller
+@ComponentScan(basePackages = "springboot.service,springboot.dao,springboot.rest,springboot.schedule")
+public class StartUpController {
+
+//	private static String[] springConfig = { "classpath:" + "/spring/context.xml" };
+//	public static final ApplicationContext context = new ClassPathXmlApplicationContext(springConfig);
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(StartUpController.class, args);
+
+	}
+
+//	@PreDestroy
+//	public void clear() {
+//		if (context != null && ((ConfigurableApplicationContext) context).isActive()) {
+//			((ConfigurableApplicationContext) context).close();
+//		}
+//	}
+
+	// @RequestMapping("/")
+	// @ResponseBody
+	// String home() {
+	// return "Hello World!";
+	// }
+
+	/*
+	 * @PostConstruct public void startDBManager() {
+	 * 
+	 * //hsqldb DatabaseManagerSwing.main(new String[] { "--url",
+	 * "jdbc:hsqldb:mem:testdb", "--user", "sa", "--password", "" });
+	 * 
+	 * //derby //DatabaseManagerSwing.main(new String[] { "--url",
+	 * "jdbc:derby:memory:testdb", "--user", "", "--password", "" });
+	 * 
+	 * //h2 //DatabaseManagerSwing.main(new String[] { "--url",
+	 * "jdbc:h2:mem:testdb", "--user", "sa", "--password", "" });
+	 * 
+	 * }
+	 */
+}
---
+++ src/main/java/springboot/rest/PersonController.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package springboot.rest;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import springboot.config.StartUpController;
+import springboot.dao.PersonService;
+import springboot.model.Person;
+
+/**
+ * @author ravikant.sharma 12-Jan-2017
+ */
+@RestController
+@RequestMapping("/pc")
+public class PersonController {
+	private final Logger logger = LogManager.getLogger(PersonController.class);
+	//private org.springframework.context.support.ReloadableResourceBundleMessageSource messageSource;
+	@Autowired
+	PersonService personService;
+	
+	
+
+	public PersonController() { 
+//		ClassPathXmlApplicationContext context = (ClassPathXmlApplicationContext) StartUpController.context;
+//		messageSource =  (ReloadableResourceBundleMessageSource) context.getBean("messageSource");
+	}
+	 
+	
+//	@RequestMapping(value = "/messageSource", method = RequestMethod.GET)
+//	public String getMessageSource(@RequestHeader(value = "lang", required = false) Locale locale) {
+//		
+//		   Object[] args = new Object[]{"askName"};
+//		String m=messageSource.getMessage("askName", args, locale);
+//		return m ;
+//	}
+	
+	@RequestMapping(value = "/addPerson", method = RequestMethod.POST)
+	public Boolean add(@RequestParam(value = "fname") String fname, @RequestParam(value = "lname") String lname,
+			@RequestParam(value = "age") int age, @RequestParam(value = "place") String place,
+			@RequestHeader(value = "lang", required = false) Locale locale) {
+		
+		Boolean isAdded=false;
+		
+		Person person = new Person();
+		person.setFirstName(fname);
+		person.setLastName(lname);
+		person.setAge(age);
+		person.setPlace(place);
+		
+		if (personService.addPerson(person) > 0) {
+			isAdded=true;
+			logger.info("Person saved successfully");
+		}else{
+			isAdded=false;
+		}
+
+		
+		return isAdded;
+	}
+
+	@RequestMapping(value = "/getPersonByName", method = RequestMethod.GET)
+	public List<Person> getPersonByName(@RequestParam(value = "fname") String fname,
+			@RequestParam(value = "lname") String lname,
+			@RequestHeader(value = "lang", required = false) Locale locale) {
+		
+		List<Person> lstperson=personService.getPersonByName(fname, lname);
+		return lstperson ;
+	}
+	
+	@RequestMapping(value = "/getAllPersons", method = RequestMethod.GET)
+	public List<Person> getAllPersons(@RequestHeader(value = "lang", required = false) Locale locale) {
+		
+		List<Person> lstperson=personService.getAllPerson();
+		return lstperson ;
+	}
+}
